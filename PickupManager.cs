a99c738c21c4069be3beab326147873435ae68b6
Josh Guempel <43886317+JoshGuempel@users.noreply.github.com>
2019-09-17 14:19:02 -0700

    Uploaded files

---
+++ PickupManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupManager : MonoBehaviour
+{
+    //This script manages the pickups of an individual car.
+    public List<Transform> attachPositions;
+    private GameObject weaponClone;
+    public int num_attachments = 0;
+    private void OnTriggerEnter(Collider other) {
+        bool done = false;
+        if(other.tag == "weapon") {
+            foreach (Transform attachPos in attachPositions) {
+                if(!done && attachPos.childCount == 0) {   
+                    weaponClone = Instantiate(other.gameObject, attachPos.position, attachPos.rotation, attachPos) as GameObject;
+                    weaponClone.transform.SetParent(attachPos);
+                    done = true; 
+                    num_attachments++;
+                }
+            }
+        }
+    }
+}
---
+++ WeaponPickup.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponPickup : MonoBehaviour
+{
+    public Renderer rend;
+    public Collider col;
+    private float timer = 0.0f;
+    public bool active = true;
+    public float respawnTime = 5.0f;
+    public float amplitude = 0.01f;
+    public float frequency = 1f;
+
+    Vector3 current_pos = new Vector3();
+
+    void Update () {
+        if(!active) {
+            if(transform.parent) {
+                rend.enabled = true;
+                col.enabled = true;
+                active = true;
+            }
+            timer += Time.deltaTime;
+            if(timer >= respawnTime) {
+                rend.enabled = true;
+                col.enabled = true;
+                active = true;
+                timer = 0.0f;
+            }
+        }
+        if(!transform.parent && active) {
+            transform.Rotate (0,35*Time.deltaTime,0);
+            current_pos = transform.position;
+            current_pos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+            transform.position = current_pos;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.transform.parent.gameObject.tag == "car") {
+            int max_a = other.transform.parent.gameObject.GetComponent<PickupManager>().attachPositions.Count;
+            int curr_a = other.transform.parent.gameObject.GetComponent<PickupManager>().num_attachments;
+            if (curr_a < max_a) {
+                active = false;
+                rend.enabled = false;
+                col.enabled = false; 
+            }
+        }
+    }
+}
